52b0092920f330bfae7fd0f22a121e1e29a5d335
wensimin <hukaizhiyu2@gmail.com>
2021-02-21 14:39:03 +0800

    初始导入

---
+++ src/main/kotlin/tech/shali/authorizationserver/AuthorizationServerApplication.kt
@@ -0,0 +1,11 @@
+package tech.shali.authorizationserver
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AuthorizationServerApplication
+
+fun main(args: Array<String>) {
+    runApplication<AuthorizationServerApplication>(*args)
+}

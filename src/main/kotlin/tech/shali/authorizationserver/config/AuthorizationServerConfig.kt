32fe2ea4fe94aaa7ad9cfae7615c22af6774e420
wensimin <hukaizhiyu2@gmail.com>
2022-03-25 19:04:45 +0800

    jwk持久化

--- src/main/kotlin/tech/shali/authorizationserver/config/AuthorizationServerConfig.kt
+++ src/main/kotlin/tech/shali/authorizationserver/config/AuthorizationServerConfig.kt
@@ -1,5 +1,6 @@
 package tech.shali.authorizationserver.config
 
+import com.nimbusds.jose.jwk.JWK
 import com.nimbusds.jose.jwk.JWKSet
 import com.nimbusds.jose.jwk.RSAKey
 import com.nimbusds.jose.jwk.source.JWKSource
@@ -9,6 +10,7 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.core.Ordered
 import org.springframework.core.annotation.Order
+import org.springframework.core.io.ClassPathResource
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration
 import org.springframework.security.oauth2.server.authorization.config.ProviderSettings
@@ -33,24 +35,34 @@ class AuthorizationServerConfig {
     }
 
     /**
-     * jwk目前与内存绑定，每次启动时创建
-     * 如果token store改为持久化，则这里也必须持久化
-     * fixme token已持久化,此处先观察 当前猜想为更换key会使客户端需要重新请求acc token
-     * fixme 2022年3月25日观察，目前配置似乎会缓存jwk，重启不会更换
+     * jwk 存在配置文件时使用配置文件，不存在时使用临时生成
      */
     @Bean
     fun jwkSource(): JWKSource<SecurityContext?> {
+        val jwkSet = JWKSet(getRsaKey())
+        return JWKSource { jwkSelector, _ -> jwkSelector.select(jwkSet) }
+    }
+
+    private fun getRsaKey(): RSAKey {
+        return ClassPathResource("jwk.json").let { it ->
+            if (!it.exists()) {
+                createRSAKey()
+            } else {
+                JWK.parse(it.file.readText()).toRSAKey()
+            }
+        }
+    }
+
+    private fun createRSAKey(): RSAKey {
         val keyPairGenerator = KeyPairGenerator.getInstance("RSA")
         keyPairGenerator.initialize(2048)
         val keyPair = keyPairGenerator.generateKeyPair()
         val publicKey = keyPair.public as RSAPublicKey
         val privateKey = keyPair.private as RSAPrivateKey
-        val rsaKey: RSAKey = RSAKey.Builder(publicKey)
+        return RSAKey.Builder(publicKey)
             .privateKey(privateKey)
             .keyID(UUID.randomUUID().toString())
             .build()
-        val jwkSet = JWKSet(rsaKey)
-        return JWKSource { jwkSelector, _ -> jwkSelector.select(jwkSet) }
     }
 
 
